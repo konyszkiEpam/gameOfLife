19f0b05a821cb58d13aa523c78ba9000f28f2dad
Krzysztof Onyszkiewicz <krzysztof_onyszkiewicz@epam.com>
2016-04-27 16:11:57 +0200

    add new project

---
+++ GameOfLife.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GameOfLife.Web.Startup))]
+namespace GameOfLife.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
